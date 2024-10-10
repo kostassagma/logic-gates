0e0636fd24e1c799d0d022b1c32f191aa5142818
Kostas <kostassagmatopoulos@gmail.com>
2024-10-11 01:31:50 +0300

    moving line

--- src/gates/gate-component.tsx
+++ src/gates/gate-component.tsx
@@ -9,7 +9,9 @@ import GateIcon from "./gate-icon";
 const GateComponent: FC<GateType> = ({ posX, posY, id, gate }) => {
   const { cameraX, cameraY } = useGlobalSettingsStore();
   const [cursorDown, setCursorDown] = useState(false);
+  const [movingFirst, setMovingFirst] = useState(false);
   const { moveNode, deleteNode } = uselogicGatesStore();
+  const [line, setLine] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     function onMouseMove(e: MouseEvent) {
@@ -19,12 +21,35 @@ const GateComponent: FC<GateType> = ({ posX, posY, id, gate }) => {
           y: e.movementY,
         });
       }
+      if (movingFirst) {
+        setLine((prevEnd) => ({
+          x: prevEnd.x + e.movementX,
+          y: prevEnd.y + e.movementY,
+        }));
+      }
     }
 
     document.addEventListener("mousemove", onMouseMove);
 
     return () => document.removeEventListener("mousemove", onMouseMove);
-  }, [cursorDown]);
+  }, [cursorDown, movingFirst]);
+
+  useEffect(() => {
+    function onMouseUp() {
+      setMovingFirst(false);
+      setCursorDown(false);
+    }
+
+    window.addEventListener("mouseup", onMouseUp);
+
+    return () => window.removeEventListener("mouseup", onMouseUp);
+  }, []);
+
+  useEffect(() => {
+    if (!movingFirst) setLine({ x: 0, y: 0 });
+  }, [movingFirst]);
+
+  console.log(line);
 
   return (
     <div
@@ -32,14 +57,6 @@ const GateComponent: FC<GateType> = ({ posX, posY, id, gate }) => {
       style={{
         left: cameraX + posX,
         top: cameraY + posY,
-      }}
-      onMouseDown={(e) => {
-        e.preventDefault();
-        setCursorDown(true);
-      }}
-      onMouseUp={(e) => {
-        e.preventDefault();
-        setCursorDown(false);
       }}
     >
       <div className="relative">
@@ -64,12 +81,55 @@ const GateComponent: FC<GateType> = ({ posX, posY, id, gate }) => {
             />
           </svg>
         </button>
+        <div
+          onMouseDown={(e) => {
+            e.preventDefault();
+            setMovingFirst(true);
+          }}
+          onMouseUp={(e) => {
+            e.preventDefault();
+            setMovingFirst(false);
+          }}
+          className="h-2 w-2 absolute left-[-0.55px] top-[25.2px] border-2 border-blue-600 hover:scale-125 transition ease-out rounded-full z-40"
+        />
+        <div className="h-2 w-2 absolute left-[-0.55px] top-[45.25px] border-2 border-blue-600 hover:scale-125 transition ease-out rounded-full z-40" />
         <GateIcon
           gate={gate}
           width={80}
           height={80}
           className="text-white absolute top-0 left-0"
+          onMouseDown={(e) => {
+            e.preventDefault();
+            setCursorDown(true);
+          }}
+          onMouseUp={(e) => {
+            e.preventDefault();
+            setCursorDown(false);
+          }}
         />
+        {movingFirst && (
+          <div
+            className={` absolute flex flex-row ${
+              line.x >= 0 ? "left-[-0.55px]" : "right-[-0.55px]"
+            } ${line.y >= 0 ? "top-[29.7px]" : "-bottom-[29.7px]"}`}
+            style={{
+              width: Math.abs(line.x),
+              height: Math.abs(line.y),
+              transform:
+                line.y >= 0 ? (line.x >= 0 ? "scale(-1, -1)" : "scale(-1, 1)") : line.x >= 0 ? "scale(1, -1)" : "scale(1, 1)",
+            }}
+          >
+            <span
+              className={`h-1/3 mb-auto w-full translate-x-[0.5px]  border-t-[1px] border-r-[1px] border-white rounded-tr-full`}
+            />
+            <span
+              className={`h-[34%] mt-auto absolute w-[1px] bg-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2`}
+            />
+            <span
+              className={`h-1/3 mt-auto w-full -translate-x-[0.5px] border-l-[1px] border-b-[1px] border-white rounded-bl-full`}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
